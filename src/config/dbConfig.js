af95432212b18f75d8768b81d4daba66e03dc430
benfocuson <focuson58383@gmail.com>
2023-07-25 03:51:21 +0700

    okzz

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -2,20 +2,8 @@ const { Sequelize, DataTypes } = require('sequelize');
 const config = require('./config.js')
 
 // Khởi tạo Sequelize với pool
-// const sequelize = new Sequelize(config.db.database, config.db.user, config.db.password, {
-//   host: config.db.host,
-//   dialect: 'mysql',
-//   dialectModule: require('mysql2'),
-//   pool: {
-//     max: 10,  // Số lượng kết nối tối đa được mở cùng một lúc
-//     min: 0,   // Số lượng kết nối tối thiểu trong pool
-//     acquire: 30000, // Thời gian tối đa để pool cố gắng kết nối với database trước khi trả lỗi
-//     idle: 10000, // Thời gian tối đa một kết nối có thể không được sử dụng trước khi được giải phóng
-//   },
-// });
-
-const sequelize = new Sequelize('bo5stkucbdrezwxwiqey', 'udjk4u0dbdyfvezs', 'zPg0yyMdzz0plgCuOKT3', {
-  host: 'bo5stkucbdrezwxwiqey-mysql.services.clever-cloud.com',
+const sequelize = new Sequelize(config.db.database, config.db.user, config.db.password, {
+  host: config.db.host,
   dialect: 'mysql',
   dialectModule: require('mysql2'),
   pool: {
@@ -25,6 +13,18 @@ const sequelize = new Sequelize('bo5stkucbdrezwxwiqey', 'udjk4u0dbdyfvezs', 'zPg
     idle: 10000, // Thời gian tối đa một kết nối có thể không được sử dụng trước khi được giải phóng
   },
 });
+
+// const sequelize = new Sequelize('bo5stkucbdrezwxwiqey', 'udjk4u0dbdyfvezs', 'zPg0yyMdzz0plgCuOKT3', {
+//   host: 'bo5stkucbdrezwxwiqey-mysql.services.clever-cloud.com',
+//   dialect: 'mysql',
+//   dialectModule: require('mysql2'),
+//   pool: {
+//     max: 10,  // Số lượng kết nối tối đa được mở cùng một lúc
+//     min: 0,   // Số lượng kết nối tối thiểu trong pool
+//     acquire: 30000, // Thời gian tối đa để pool cố gắng kết nối với database trước khi trả lỗi
+//     idle: 10000, // Thời gian tối đa một kết nối có thể không được sử dụng trước khi được giải phóng
+//   },
+// });
 
 // const sequelize = new Sequelize(config.db);
 
